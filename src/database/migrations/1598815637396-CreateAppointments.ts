44778c126a3163eb43a31a2b76c4db3be8dda1e3
Fabricyo Barreto <bass.barreto@gmail.com>
2020-08-30 17:40:18 -0300

    the code was refactored to adapt to the use of typeorm and database.

--- src/database/migrations/1598815637396-CreateAppointments.ts
+++ src/database/migrations/1598815637396-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1598815637396
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
