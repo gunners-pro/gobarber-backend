2ce270574cc1ea64771788b5044b6e2c5b98565d
Fabricyo Barreto <bass.barreto@gmail.com>
2020-11-25 17:36:03 -0300

    The appointment repository was restructured using the Liskov substitution principle.

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,7 +2,7 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
