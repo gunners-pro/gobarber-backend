81f7acd085d072680d7fd1b1be27d1f33009e7e1
Fabricyo Barreto <bass.barreto@gmail.com>
2020-11-28 09:07:53 -0300

    the first test in the application was made using jest, responsible for testing the creation of an appointment. And a fakeRepository to replace the database.

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => { });
 });
