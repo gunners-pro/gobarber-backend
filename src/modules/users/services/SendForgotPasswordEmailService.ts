ea67e523e5f583972e775d3ca5df4706514b25b3
Fabricyo Barreto <bass.barreto@gmail.com>
2020-12-01 20:06:37 -0300

    It has been encrypted that if the user does not exist, they will not be able to recover the password. and verified that the person who requested the password change was actually the account holder.

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { injectable, inject } from 'tsyringe';
-import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -16,10 +16,21 @@ class SendForgotPasswordEmailService {
     private usersRepository: IUsersRepository,
 
     @inject('MailProvider')
-    private mailProvider: IMailProvider
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository
   ) { }
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.')
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.')
   }
 }
