8db7d38ae4fdf240bb52040632716b0177c680aa
Fabricyo Barreto <bass.barreto@gmail.com>
2020-12-09 20:51:40 -0300

    Some changes were made to the business rule of the service for creating appointments, preventing a user from making an appointment with himself or on past dates, in addition to providing valid times for the application (8am to 6pm).

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { startOfHour } from 'date-fns';
+import { getHours, isBefore, startOfHour } from 'date-fns';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
@@ -20,6 +20,18 @@ class CreateAppointmentService {
 
   public async execute({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError('You can\'t create an appointment on a past date.');
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError('You can\'t create an appointment with yourself');
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError('You can only create appointments between 8am and 5pm');
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
