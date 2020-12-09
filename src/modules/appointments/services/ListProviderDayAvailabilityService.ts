37989b844cab8f73e7696b6adecaf7a6f5827b1f
Fabricyo Barreto <bass.barreto@gmail.com>
2020-12-09 17:09:40 -0300

    In the functionality of listing the availability of a day, another business rule was added to prevent an appointment from being made on a date that has already passed.

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -32,14 +32,18 @@ class ListProviderDayAvailabilityService {
 
     const eachHourArray = Array.from({ length: 10 }, (_, index) => index + hourStart);
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       }
     });
 
