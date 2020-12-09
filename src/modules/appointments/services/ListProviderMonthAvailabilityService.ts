fe51a051219633a811f8cd6c2d575f6c80e6a35f
Fabricyo Barreto <bass.barreto@gmail.com>
2020-12-08 21:18:44 -0300

    he test and the functionality to list the available days within a month has been completed.

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 
+import { getDate, getDaysInMonth } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -21,14 +22,30 @@ class ListProviderMonthAvailabilityService {
     private appointmentsRepository: IAppointmentsRepository,
   ) { }
 
-  public async execute({ provider_id, month, year }: IRequest): Promise<IResponse> {
+  public async execute({ provider_id, year, month }: IRequest): Promise<IResponse> {
     const appointments = await this.appointmentsRepository.findAllInMonthFromProvider({
       provider_id, year, month
     });
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      }
+    });
+
+    return availability;
   }
 }
 
